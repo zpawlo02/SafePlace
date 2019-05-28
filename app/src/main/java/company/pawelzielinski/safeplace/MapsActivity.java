c21f10542e1f8e883f6660be8a7488e601367cc4
zpawlo02 <zpawlo02@gmail.com>
2019-05-28 02:00:14 +0200

    circle added (have to make function to resize circle and make it draggable) two buttons added to maps fragment

--- app/src/main/java/company/pawelzielinski/safeplace/MapsActivity.java
+++ app/src/main/java/company/pawelzielinski/safeplace/MapsActivity.java
@@ -1,5 +1,6 @@
 package company.pawelzielinski.safeplace;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -7,12 +8,15 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Circle circle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +29,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng mCircleCenter = new LatLng(-34, 151);
+        circle = mMap.addCircle(new CircleOptions()
+                .strokeWidth(4)
+                .radius(250)
+                .center(mCircleCenter)
+                .strokeColor(Color.parseColor("#D1C4E9"))
+                .fillColor(Color.parseColor("#657C4DFF")));
 
+        mMap.setOnCircleClickListener(new GoogleMap.OnCircleClickListener() {
+
+            @Override
+            public void onCircleClick(Circle circle) {
+                // Flip the r, g and b components of the circle's
+                // stroke color.
+                int strokeColor = circle.getStrokeColor() ^ 0x00ffffff;
+                circle.setStrokeColor(strokeColor);
+            }
+        });
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
+        mMap.addCircle(new CircleOptions()
+                .strokeWidth(4)
+                .radius(250)
+                .center(mCircleCenter)
+                .strokeColor(Color.parseColor("#D1C4E9"))
+                .fillColor(Color.parseColor("#657C4DFF")));
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
